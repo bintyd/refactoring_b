63697c9baca220b28600fe5078edc3f5adc50af7
bintydiaby <binty200625@gmail.com>
2025-11-24 03:24:02 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,26 @@
 package theater;
 
+/**
+ * Represents a play to be performed.
+ *
+ * @null name and type must not be null
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
     }
 }
